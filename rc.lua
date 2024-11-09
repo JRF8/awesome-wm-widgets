c4d7fa1a6ae69d29bcddf5739fccd4f19a46a326
jesse <jesse.frazier.8@gmail.com>
2024-11-08 23:12:52 -0500

    preserve active tag after restart

--- rc.lua
+++ rc.lua
@@ -626,3 +626,40 @@ end)
 
 -- startup script
 awful.spawn.with_shell(".config/awesome/awesomestart.sh")
+
+-- code from https://www.reddit.com/r/awesomewm/comments/syjolb/preserve_previously_used_tag_between_restarts/?rdt=59007
+-- preserve active tag after restart
+awesome.connect_signal("exit", function(reason_restart)
+	if not reason_restart then
+		return
+	end
+
+	local file = io.open("/tmp/awesomewm-last-selected-tags", "w+")
+
+	for s in screen do
+		file:write(s.selected_tag.index, "\n")
+	end
+
+	file:close()
+end)
+
+awesome.connect_signal("startup", function()
+	local file = io.open("/tmp/awesomewm-last-selected-tags", "r")
+	if not file then
+		return
+	end
+
+	local selected_tags = {}
+
+	for line in file:lines() do
+		table.insert(selected_tags, tonumber(line))
+	end
+
+	for s in screen do
+		local i = selected_tags[s.index]
+		local t = s.tags[i]
+		t:view_only()
+	end
+
+	file:close()
+end)
